d17883584a1d4baba5e1167ee9755233319ee9f8
Thanura gayanaka <57209475+ThanuraGayanaka@users.noreply.github.com>
2025-03-15 16:49:24 +0530

    Added new testcases

--- src/test/java/TestPages/LoginTest.java
+++ src/test/java/TestPages/LoginTest.java
@@ -5,6 +5,7 @@ import Pages.LoginPage;
 import Utility.ConfigReader;
 import Utility.ScreenshotUtil;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTestpage {
@@ -19,12 +20,12 @@ public class LoginTest extends BaseTestpage {
 
            String configUsername = ConfigReader.getProperty("Username");
 
-           System.out.println( ConfigReader.getProperty("Username"));
+          // System.out.println( ConfigReader.getProperty("Username"));
 
            String configPassword = ConfigReader.getProperty("Password");
 
 
-           System.out.println( ConfigReader.getProperty("Password"));
+          // System.out.println( ConfigReader.getProperty("Password"));
 
 
            loginPage.enterUsername(configUsername);
@@ -45,6 +46,41 @@ public class LoginTest extends BaseTestpage {
            throw new RuntimeException("file reader issue");
        }
 
+
+    }
+@Test
+    public void validate_Url(){
+
+
+        // After navigating to the page
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = ConfigReader.getProperty("Homepage_url");
+
+
+        Assert.assertEquals(actualUrl,expectedUrl,"correctly navigate to the dashboard page");
+
+        ScreenshotUtil.takeScreenshot(driver,"Navigate dashboard page");
+
+
+    }
+    @Test(priority = 2)
+    public void checkInvalid_username() throws InterruptedException {
+
+        LoginPage loginPage= new LoginPage(driver);
+        loginPage.enterUsername(ConfigReader.getProperty("InvalidUsername"));
+        loginPage.enterpassword(ConfigReader.getProperty("Password"));
+        loginPage.clickLoginBtn();
+
+        Thread.sleep(2000);
+
+
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = ConfigReader.getProperty("Homepage_url");
+
+
+        Assert.assertNotEquals(actualUrl,expectedUrl,"Login function failed");
+
+        ScreenshotUtil.takeScreenshot(driver,"Login fail");
 
     }
 }
